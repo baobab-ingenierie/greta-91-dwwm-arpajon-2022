e980a56baf5e76190f3cafcf472b5dce7eaa2aee
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-01-07 10:07:49 +0100

    CP5 Day 4

--- cp5/mpd-arpajon.sql
+++ cp5/mpd-arpajon.sql
@@ -14,11 +14,35 @@ SHOW DATABASES
 USE arpajon
 ;
 
+-- Suppression de la table si elle existe
+DROP TABLE IF EXISTS arpajon.students
+;
+
 -- Création de la table STUDENTS
 CREATE TABLE arpajon.students(
-	id_stud INTEGER,
-    fname VARCHAR(30)
-);
+	id_stud TINYINT PRIMARY KEY AUTO_INCREMENT,
+    fname VARCHAR(30) NOT NULL,
+    dob DATE,
+    sex ENUM('F', 'M') NOT NULL
+)
+ENGINE=InnoDB 
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci
+;
+
+-- Création de la table TEACHERS
+-- id_teach TINYINT PK AI
+-- fname VARCHAR NN
+-- grade TINYINT (valeur entre 1 et 5)
+-- begin DATE
+-- email VARCHAR UQ
+
+
+-- Vérif création tables
+SHOW TABLES
+;
+
+
 
 
 
