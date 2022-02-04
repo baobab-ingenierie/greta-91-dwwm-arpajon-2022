04bdadc7621c8239372c67f796eef2647e3f2ace
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-02-04 18:49:46 +0100

    CP6 Day 2

--- cp6/langage-sql-exos.sql
+++ cp6/langage-sql-exos.sql
@@ -127,3 +127,106 @@ FROM students s JOIN follow f ON s.id_stud = f.id_stud
 				JOIN courses c ON f.id_cur = c.id_cur
 WHERE s.id_stud = 14
 ;
+
+-- ##################################################
+-- TP 2-5 : jointures externes
+-- ##################################################
+
+-- Lister les profs associés à leur matières (ou pas)
+SELECT t.fname, c.title
+FROM teachers t JOIN courses c ON t.id_teach = c.id_teach
+; -- interne
+
+SELECT t.fname, c.title
+FROM teachers t LEFT JOIN courses c ON t.id_teach = c.id_teach
+; -- externe à gauche
+
+SELECT t.fname, c.title
+FROM teachers t RIGHT JOIN courses c ON t.id_teach = c.id_teach
+; -- externe à droite
+
+SELECT t.fname, c.title
+FROM teachers t LEFT JOIN courses c ON t.id_teach = c.id_teach
+UNION
+SELECT t.fname, c.title
+FROM teachers t RIGHT JOIN courses c ON t.id_teach = c.id_teach
+; -- simulation d'un full
+
+-- ##################################################
+-- TP 3-1 : fonctions
+-- ##################################################
+
+-- Afficher le prénom en majuscule et la date de naissance 
+-- des étudiants ayant plus de 30 ans
+SELECT UPPER(fname) AS prenom,
+		dob
+FROM students
+WHERE FLOOR(ABS(DATEDIFF(dob, NOW())) / 365.25) >= 30
+;
+
+-- A quelle date chaque étudiant partira à la retraite (62 ans)
+-- et combien d'années cela représente par arpport à aujourd'hui
+SELECT UPPER(fname) AS prenom,
+		DATE_ADD(dob, INTERVAL 62 YEAR) date_depart,
+        FLOOR(DATEDIFF(DATE_ADD(dob, INTERVAL 62 YEAR), NOW())/365.25) AS nb_annees
+FROM students
+;
+
+-- Afficher le deuxième mot du titre de chaque cours
+-- Aide : LOCATE, LENGTH et SUBSTR
+SELECT SUBSTR(
+	title, 
+    LOCATE(' ', title) + 1, 
+    LOCATE(' ', title, LOCATE(' ', title) + 1) - (LOCATE(' ', title) + 1)
+    ) AS 2nd_mot
+    , title
+FROM courses
+;
+-- REGEXP_SUBSTR
+
+-- ##################################################
+-- TP 3-2 : regroupements (GROUP BY ... HAVING)
+-- ##################################################
+
+-- Afficher la moyenne (AVG) des notes obtenues par les 
+-- filles sur le cours CP5
+SELECT AVG(score) AS moyenne
+FROM students s JOIN follow f ON s.id_stud = f.id_stud
+WHERE sex = 'F'
+-- AND id_cur LIKE '%CP5%'
+AND id_cur = 'CP5'
+;
+
+-- Qui a obtenu la meilleure note (MAX) en CP4 chez
+-- les garçons ?
+SELECT s.fname, MAX(score) AS meilleure_note -- exécutée en 5ème
+FROM follow f JOIN students s ON f.id_stud = s.id_stud -- exécutée en 1er
+WHERE sex = 'M' AND id_cur = 'CP4' -- exécutée en 2ème
+GROUP BY s.fname -- exécutée en 3ème
+HAVING MAX(score) > 19 -- exécutée en 4ème
+-- HAVING meilleure_note > 19 -- exécutée en 4ème / PROPRE A MYSQL
+ORDER BY meilleure_note DESC -- exécutée en 6ème
+LIMIT 3
+;
+-- Qui des filles ou des garçons ont la meilleure
+-- moyenne générale (AVG) ?
+SELECT s.sex, AVG(f.score) AS moyenne
+FROM students s JOIN follow f ON s.id_stud = f.id_stud
+GROUP BY s.sex
+;
+
+-- Moyenne des notes par génération
+SELECT TRUNCATE(YEAR(s.dob), -1) AS decennie, AVG(f.score) AS moyenne
+FROM students s JOIN follow f ON s.id_stud = f.id_stud
+GROUP BY TRUNCATE(YEAR(s.dob), -1) 
+;
+
+-- Quel formateur/trice a le meilleur rendement : 
+-- meilleure moyenne (AVG) sur les notes obtenues par 
+-- leurs élèves ?
+SELECT t.fname, AVG(f.score) AS moyenne
+FROM  courses c JOIN follow f ON c.id_cur = f.id_cur
+				JOIN teachers t ON t.id_teach = c.id_teach
+GROUP BY t.fname
+ORDER BY moyenne DESC LIMIT 1
+;
--- cp6/langage-sql-td.sql
+++ cp6/langage-sql-td.sql
@@ -107,3 +107,144 @@ FROM courses c JOIN teachers t USING(id_teach)
 SELECT c.title, t.fname
 FROM courses c NATURAL JOIN teachers t 
 ; -- jointure naturelle (peu recommandée)
+
+-- Fonctions numériques
+SELECT ROUND(1234.56789), 
+		ROUND(1234.1223),
+        ROUND(3.14159, 2),
+        ROUND(3.14159, 3),
+		ROUND(1789.56, -2)
+;
+
+SELECT CEIL(1234.00000001),
+		CEIL(1234.999855479999)
+; -- entier supérieur
+
+SELECT FLOOR(1234.00000001),
+		FLOOR(1234.999855479999)
+; -- entier inférieur
+
+SELECT TRUNCATE(1234.56789, 2),
+		TRUNCATE(1294.56789, -2)
+; -- troncature
+
+-- Fonctions date et heure
+SELECT DAY('1998-07-12'),
+		MONTH('1998-07-12'),
+        YEAR('1998-07-12'),
+		HOUR('1998-07-12 22:50:16'),
+		MINUTE('1998-07-12 22:50:16'),
+		SECOND('1998-07-12 22:50:16')
+;
+
+SELECT NOW(),
+		CURRENT_TIMESTAMP(),
+        DATE_ADD(NOW(), INTERVAL 15 MONTH)
+;
+
+SELECT DATEDIFF('1998-07-12', '2018-07-15'),
+		DATEDIFF('2018-07-15', '1998-07-12'), -- en jours
+		DATEDIFF('2018-07-15', '1998-07-12') / 365.25,
+        FLOOR(DATEDIFF('2018-07-15', '1998-07-12') / 365.25)
+;
+
+-- Fonctions chaine de caractères
+SELECT UPPER('Hello world!'), -- UCASE
+		LOWER('Hello world!'), -- LCASE
+        SUBSTR('Toto aime les gâteaux', 6),
+        SUBSTR('Toto aime les gâteaux', 6, 4), -- SUBSTRING
+        LENGTH('Tata aime le chocolat noir'),
+        INSTR('Titi fait cuicui', 'i'),
+        LOCATE('u', 'Tutu m''a vu', 5)
+;
+
+SELECT 'Toto mange trop' AS sentence,
+		-- SUBSTR('Toto mange trop', 6, 5),
+		-- SUBSTR('Toto bouffe trop', 6, 5),
+		SUBSTR('Emmanuel mange trop', LOCATE(' ', 'Emmanuel mange trop') + 1 , 5),
+		SUBSTR('Toto bouffe trop', LOCATE(' ', 'Toto bouffe trop') + 1 , 5),
+		LOCATE(' ', 'Toto bouffe trop', LOCATE(' ', 'Toto bouffe trop') + 1),
+        SUBSTR(
+			'Toto bouffe trop', 
+            LOCATE(' ', 'Toto bouffe trop') + 1, 
+            LOCATE(' ', 'Toto bouffe trop', LOCATE(' ', 'Toto bouffe trop') + 1) - (LOCATE(' ', 'Toto bouffe trop') + 1)
+		)
+;
+
+-- Structure CASE simple
+SELECT sex,
+		CASE sex
+			WHEN 'F' THEN 'Madame'
+			WHEN 'M' THEN 'Monsieur'
+            ELSE 'Pané'
+        END AS civilite,
+		fname,
+		dob
+FROM students
+;
+
+-- Structure CASE élaborée
+/*
+SELECT fname,
+		dob,
+        CASE MONTH(dob)
+			CASE 1 THEN 1
+			CASE 2 THEN 1
+			CASE 3 THEN 1
+            ...
+            CASE 12 THEN 4
+            ELSE 0
+        END AS trimestre,
+FROM students
+;
+*/
+
+SELECT fname,
+		dob,
+        CASE
+			WHEN MONTH(dob) < 4 THEN 1
+			WHEN MONTH(dob) < 7 THEN 2
+			WHEN MONTH(dob) < 10 THEN 3
+			WHEN MONTH(dob) < 13 THEN 4
+            ELSE 0
+        END AS trimestre
+FROM students
+;
+-- Opérations ensemblistes
+
+-- UNION
+SELECT CONCAT('T', id_teach) AS code,  fname, dos
+FROM teachers
+UNION
+SELECT CONCAT('S', id_stud), fname, dob 
+FROM students
+;
+
+-- INTERSECTION : prénoms en commun dans les tables
+INSERT INTO teachers(id_teach, fname, grade, dos, email)
+VALUES(7, 'Emmanuel', 1, '2022-02-04', 'maniasse@matheux.fr')
+;
+
+SELECT fname 
+FROM teachers t
+WHERE EXISTS (SELECT fname 
+				FROM students s
+                WHERE s.fname = t.fname)
+;
+
+-- EXCEPTION : prénoms dans une table mais pas dans l'autre
+SELECT fname 
+FROM teachers t
+WHERE NOT EXISTS (SELECT fname 
+				FROM students s
+                WHERE s.fname = t.fname)
+;
+-- Inverse
+SELECT fname 
+FROM students s
+WHERE NOT EXISTS (SELECT fname 
+				FROM teachers t
+                WHERE s.fname = t.fname)
+;
+
+
