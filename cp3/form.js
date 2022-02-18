0e9ec690910b63fb4237484fac9f373c5ce643ed
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-02-18 19:10:16 +0100

    CP6 Day 6

--- cp3/form.js
+++ cp3/form.js
@@ -19,3 +19,116 @@ document.querySelector('#exp').addEventListener(
         document.querySelector('#expVal').textContent = expertise(e.target.value);
     }
 );
+
+/**
+ * Evénément onclick sur le bouton de sauvegarde locale
+ * https://developers.google.com/maps/documentation/javascript/geocoding
+ */
+
+document.querySelector('#save').addEventListener(
+    'click',
+    (evt) => {
+        let inputs = document.querySelectorAll('#candidate input:not([type=hidden])[name]'), candidate = {}, jobs = [];
+        console.log(inputs);
+
+        // méthode forEach
+        inputs.forEach((input) => {
+            // console.log(input.type);
+            if (input.type === 'radio') {
+                if (input.checked) {
+                    candidate[input.name] = input.value;
+                }
+            } else if (input.type === 'checkbox') {
+                if (input.checked) {
+                    jobs.push(input.value);
+                }
+            } else {
+                candidate[input.name] = input.value;
+            }
+        });
+
+        candidate.jobs = jobs;
+        // console.log(candidate);
+
+        // Stockage local de l'objet sérialisé (converti en string)
+        let key = document.querySelector('#fname').value;
+        if (key !== '') {
+            localStorage.setItem(key, JSON.stringify(candidate));
+            alert('Stockage local terminé avec succès.');
+            console.log(evt);
+            evt.preventDefault(); // stoppe l'événement en cours
+        } else {
+            alert('Vous devez saisir un prénom !');
+        }
+    }
+);
+
+/**
+ * Evénément ondomcontentloaded sur le document (qd doc chargé)
+ * AJAX avec fetch()
+ */
+
+document.addEventListener(
+    'DOMContentLoaded',
+    function () {
+        // Vide la liste
+        let select = document.querySelector('#region'), option;
+        select.innerHTML = '';
+
+        // Lance la requête AJAX via URL vers API REST
+        fetch('https://geo.api.gouv.fr/departements/83/communes')
+            // Méthode de promesse
+            .then(function (resp) {
+                return resp.json();
+            })
+            // Exploite les data
+            .then(function (data) {
+                // console.log(data);
+                data.forEach(function (obj) {
+                    // console.log(obj);
+                    option = document.createElement('option');
+                    option.value = obj.codesPostaux[0];
+                    option.textContent = obj.nom;
+                    select.appendChild(option);
+                })
+            })
+            // Si erreur
+            .catch(function (err) {
+                console.warn(err);
+            })
+    }
+);
+
+/**
+ * Evénément onchange de la liste des communes
+ * DHTML (Dynamic HTML)
+ */
+
+document.querySelector('#region').addEventListener(
+    'change',
+    function (evt) {
+        // Options d'affichage
+        let options = {
+            // center: new google.maps.LatLng(48.52, 2.19),
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // ou HYBRID, SATELLITE ou TERRAIN
+            zoom: 15,
+            scrollWheel: false
+        };
+
+        // Localisation adresse
+        let map = new google.maps.Map(document.querySelector('#carte'), options);
+        let geocoder = new google.maps.Geocoder();
+        geocoder.geocode(
+            { 'address': evt.target.value + ',France' },
+            function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location); // A AJOUTER !
+                    let marker = new google.maps.Marker({
+                        position: results[0].geometry.location.latLng,
+                        map: map
+                    });
+                }
+            }
+        );
+    }
+);
--- cp3/objects.js
+++ cp3/objects.js
@@ -41,15 +41,112 @@ class Person {
 
     // attribut privé
     #size = 0;
+    #lang = ['FR'];
 
-    constructor(newFname, newDob, newSize = 1.69) {
+    // constructeur
+    constructor(newFname, newDob) {
         this.fname = newFname;
         this.dob = newDob;
-        this.#size=newSize; // A modifier avec mutateur
     }
+
+    // accesseurs ou getters : 
+    // récupère la valeur d'un attribut privé (encapsulation)
+    getSize = function () {
+        return this.#size;
+    };
+
+    getLang = function () {
+        return this.#lang;
+    };
+
+    getAge = function () {
+        return Math.floor((Date.now() - new Date(this.dob)) / 1000 / 3600 / 24 / 365.25);
+    };
+
+    /**
+     * mutateurs ou setters :
+     * change la valeur d'un attribut privé (encapsulation)
+     * @param {number} newSize taille en mètres
+     * @return {void}
+     */
+
+    setSize = function (newSize) {
+        // teste si nombre
+        if (isNaN(newSize)) {
+            throw 'Vous devez indiquer un nombre.';
+        }
+
+        // teste si taille est comprise entre 50cm et 2m72
+        if (newSize < .5 || newSize > 2.72) {
+            throw 'La taille doit être comprise entre 50cm et 2m72';
+        }
+
+        this.#size = newSize;
+    };
+
+    /**
+     * Langues parlées par la personne
+     * @param {string} newLang nouvelle langue parlée
+     * @return {void}
+     */
+
+    setLang = function (newLang) {
+        // teste si le code ISO 639 (langues) est correctement formatté
+        // ^[A-Za-z]{2}$
+        if (!newLang.match(/^[A-Za-z]{2}$/)) {
+            throw 'Le code doit correspondre exactement à deux caractères alpha.'
+        }
+
+        this.#lang.push(newLang.toUpperCase());
+    };
+
+    // Méthode publique
+
+    /**
+     * Faire dire "Salut" à la personne
+     * @param {boolean} all (true) pour toutes les langues (false) pour la 1ère
+     * @return {void} salutations dans une ou plusieurs langues
+     */
+
+    sayHello = function (all = false) {
+        let end = all ? this.#lang.length : 1;
+        for (let i = 0; i < end; i++) {
+            console.info(this.#getHello(this.#lang[i]));
+        }
+    };
+
+    #getHello = function (curr) {
+        // Renvoie "Salut" dans la langue parlée par la personne
+        switch (curr) {
+            case 'FR':
+                return 'Salut !';
+            case 'EN':
+                return 'Hello !';
+            case 'ES':
+                return 'Holà !';
+            case 'DE':
+                return 'Halo !';
+            case 'IT':
+                return 'Ciao !';
+            case 'AR':
+                return 'Salam !';
+            default:
+                return 'Wesh ma gueule !';
+        }
+    };
 }
 
 let obj2 = new Person('Quentin', '1995-11-02');
+console.info(obj2.getAge());
+
+obj2.setSize(1.65);
+console.info(obj2.getSize());
+
+obj2.setLang('EN');
+obj2.setLang('JP');
+console.info(obj2.getLang());
+
+obj2.sayHello();
+obj2.sayHello(true);
+
 console.log(obj2);
-// obj2.fname = 'Tarentino';
-// console.log(obj2);
